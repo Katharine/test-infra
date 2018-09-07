2b2fb9622914de632ceaf8e1be048e23536d2b2b
Katharine Berry <ktbry@google.com>
2018-09-07 16:51:31 -0700

    Better command line interface.

--- coverage/main.go
+++ coverage/main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"golang.org/x/tools/cover"
-	"./pkg/cov"
-	"os"
+	"log"
+	"github.com/spf13/cobra"
+	"k8s.io/test-infra/coverage/cmd/diff"
+	"k8s.io/test-infra/coverage/cmd/merge"
 )
 
+var rootCommand = &cobra.Command{
+	Use: "coverage",
+	Short: "coverage is a tool for manipulating Go coverage files.",
+}
+
+func run() error {
+	rootCommand.AddCommand(diff.MakeCommand())
+	rootCommand.AddCommand(merge.MakeCommand())
+	return rootCommand.Execute()
+}
+
+
 func main() {
-	before, err := cover.ParseProfiles(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-	after, err := cover.ParseProfiles(os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-	diff, err := cov.DiffProfiles(before, after)
-	if err := cov.DumpProfile(diff, os.Stdout); err != nil {
-		panic(err)
+	if err := run(); err != nil {
+		log.Fatal(err)
 	}
 }
